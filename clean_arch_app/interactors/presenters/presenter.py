a52bda2b850f403aab10972342906ad0cde08d05
Deepak Kumar Malladi <kapeed2091@gmail.com>
2019-08-03 15:57:18 +0530

    US1: Added abstract methods related to storage and presenter required. While implementing interactor we need not worry about implementation of other layer components. Its the responsibility of developers who implement storage, presenter layers to implement by taking the abstract methods as reference.

--- clean_arch_app/interactors/presenters/presenter.py
+++ clean_arch_app/interactors/presenters/presenter.py
@@ -4,8 +4,13 @@ Created on 2019-07-18
 @author: kapeed2091
 """
 import abc
+from clean_arch_app.interactors.storages.post_storage import PostDTO
 
 
 class Presenter:
 
     __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def create_post(self, post_dto: PostDTO):
+        pass
--- clean_arch_app/interactors/storages/post_storage.py
+++ clean_arch_app/interactors/storages/post_storage.py
@@ -4,8 +4,22 @@ Created on 2019-07-18
 @author: kapeed2091
 """
 import abc
+import datetime
+from dataclasses import dataclass
+
+
+@dataclass
+class PostDTO:
+    post_id: int
+    content: str
+    created_at: datetime.datetime
+    created_by_id: int
 
 
 class PostStorage:
 
     __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def create_post(self, content: str, created_by_id: int) -> PostDTO:
+        pass
