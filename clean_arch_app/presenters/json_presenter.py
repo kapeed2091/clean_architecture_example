dbf7157abb954e064ffdc663589ce69f99709252
Deepak Kumar Malladi <kapeed2091@gmail.com>
2019-08-03 16:22:30 +0530

    "US1: Implementing json presenter for 'create_post'. While implementing interactor we listed an abstract method in presenter which we now implement its actual functionality .

--- clean_arch_app/presenters/json_presenter.py
+++ clean_arch_app/presenters/json_presenter.py
@@ -4,9 +4,12 @@ Created on 2019-07-18
 @author: kapeed2091
 """
 
-
+from clean_arch_app.interactors.storages.post_storage import PostDTO
 from clean_arch_app.interactors.presenters.presenter import Presenter
 
 
 class JsonPresenter(Presenter):
-    pass
+    def create_post(self, post_dto: PostDTO):
+        return {
+            'post_id': post_dto.post_id
+        }
