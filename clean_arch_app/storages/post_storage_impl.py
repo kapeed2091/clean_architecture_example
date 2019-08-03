f062fca07af8e004297b222bec69c99f7890a7b9
Deepak Kumar Malladi <kapeed2091@gmail.com>
2019-08-03 16:20:20 +0530

    "US1: Implementing storage for 'create_post'. While implementing interactore we listed an abstract method in storage which we now implement its actual functionality.

--- clean_arch_app/storages/post_storage_impl.py
+++ clean_arch_app/storages/post_storage_impl.py
@@ -3,8 +3,18 @@ Created on 2019-07-18
 
 @author: kapeed2091
 """
+from clean_arch_app.interactors.storages.post_storage import PostDTO
 from clean_arch_app.interactors.storages.post_storage import PostStorage
 
 
 class PostStorageImpl(PostStorage):
-    pass
+
+    def create_post(self, content: str, created_by_id: int) -> PostDTO:
+        from clean_arch_app.models import Post
+        post_obj = Post.objects.create(content=content,
+                                       created_by_id=created_by_id)
+        post_dto = PostDTO(
+            post_obj.id, post_obj.content, post_obj.created_at,
+            post_obj.created_by_id
+        )
+        return post_dto
